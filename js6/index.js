61b03363edf05bc6fb1b1c0dbe15c7a3d4311b15
Sophia de Jong <Sophia@Sophia-Marina-de-Jong-3.local>
2022-09-22 13:50:59 +0200

    calculator

--- js6/index.js
+++ js6/index.js
@@ -2,4 +2,33 @@ import ps from "prompt-sync"
 
 const prompt = ps();
 
-const num = prompt("Escribe un numero: ");
+while (true) {
+    console.log('1 - Salir')
+    console.log('2 - Sumar')
+    console.log('3 - Restar')
+    console.log('4 - Multiplicar')
+    console.log('4 - Dividir')
+
+    const opt = prompt('Escribe una opcion:');
+    if (opt == '1') break;
+    const n1 = prompt("Escribe un numero: ");
+    const n2 = prompt("Escribe un numero: ");
+
+    switch (opt) {
+        case '2':
+            console.log(parseInt(n1) + parseInt(n2))
+            break;
+        case '3':
+            console.log(parseInt(n1) - parseInt(n2))
+            break;
+        case '4':
+            console.log(parseInt(n1) * parseInt(n2))
+            break;
+        case '5':
+            console.log(parseInt(n1) / parseInt(n2))
+            break;
+    }
+
+    
+
+}
