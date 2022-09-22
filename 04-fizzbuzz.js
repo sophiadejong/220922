9fb0600f8da605c85696e1e02ba84b8aae61afc4
Sophia de Jong <Sophia@Sophia-Marina-de-Jong-3.local>
2022-09-22 11:37:04 +0200

    palindromo

--- 04-fizzbuzz.js
+++ 04-fizzbuzz.js
@@ -5,15 +5,10 @@
 
 const fizzbuzz = () => {
     for(let i = 0;i < 100;i++){
-    if (i%3== 0) {
-        console.log("fuzz")       
+    if (i%3== 0) console.log("fuzz")       
+    else if (i%5== 0) console.log("buzz")
+     else console.log(i)
     }
-    else if (i%5== 0) {
-        console.log("buzz")
-    } else {
-        console.log(i)
-    }
-}
 }
 
 fizzbuzz()
