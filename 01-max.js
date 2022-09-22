49b4cdc1c642adb6113ca9ddd3e3ed678493db43
Sophia de Jong <Sophia@Sophia-Marina-de-Jong-3.local>
2022-09-22 10:42:26 +0200

    testout

--- 01-max.js
+++ 01-max.js
@@ -3,7 +3,7 @@
 const max = (n1,n2) => {
     if (n1>n2) return n1
     return n2
-    } 
-}
+} 
+
 
 console.log(Math.max(5, 10));
--- 03-maxArrays.js
+++ 03-maxArrays.js
@@ -1,8 +1,13 @@
 // funcion que retorne en num max de un array
-
-const maxArr = (nums) => {
-
+const maxArr = (nums) =>{
+    let numMax = nums [0];
+    for (let i = 0; i < nums.length; i++) {
+        if(nums[i] > numMax) {
+            numMax = nums[i]           
+        }   
+    }
+    return numMax    
 }
 
-console.log(maxArr [3,1,7,0,-90])
+console.log(maxArr ([3,1,7,0,-90]))
 
